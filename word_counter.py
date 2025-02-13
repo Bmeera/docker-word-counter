e155fcc789907e77bd5eb5692174a29587577a25
Ameerat Bello <bmeera13@gmail.com>
2025-02-12 21:11:20 -0500

    Refactored word_counter.py for better interactivity and readability

--- word_counter.py
+++ word_counter.py
@@ -1,16 +1,22 @@
 # File: word_counter.py
 
 # Define the filename where results will be saved
-filename = "word_count.txt"
+filename = "/app/data/word_count.txt"
+
+# Welcome Message
+print("Welcome to Word Counter!\nThe console application that counts your words.")
+
+print("No matter how lenghty, convoluted, or you know \"uncountable\" the words are, Word Counter can count them!")
 
 # Get user input
-text = input("Enter a sentence: ")
+text = input("To begin, please enter the sentence: ")
 
 # Count the number of words
 word_count = len(text.split())
+print(f"The word count is {word_count} ")
 
 # Save the result in a text file
 with open(filename, "w") as f:
-    f.write(f"Word count: {word_count}\n")
+    f.write(f"The word count is {word_count}\n")
 
 print(f"Word count saved to {filename}")
